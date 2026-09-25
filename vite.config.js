9c11c060738ded54a61c10140d73e5a4b765d7cb
Darshan Sarode <darshan.sarode@proton.me>
2026-09-25 17:30:51 +0530

    Remove comment and update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+/Changthang-GIS/
 export default defineConfig({
   plugins: [react()],
 })
