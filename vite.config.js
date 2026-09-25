32128df25819735c0056fc5d83b637c2cf6dc2f9
Darshan Sarode <darshan.sarode@proton.me>
2026-09-25 17:32:59 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-/Changthang-GIS/
+
 export default defineConfig({
   plugins: [react()],
+  base:"/Changthang-GIS/"
 })
